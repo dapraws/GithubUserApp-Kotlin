4760fdbff540689ca563eb979fc790a882d1a064
dapraws <darrelprawira26@gmail.com>
2024-03-10 19:45:42 +0700

    first commit

---
+++ app/src/main/java/com/example/githubuserapp_kotlin/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubuserapp_kotlin.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubuserapp_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
